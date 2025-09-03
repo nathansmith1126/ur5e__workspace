a429bd7181543bdbd8b25b910c0f3d73ac5a2f4e
nathansmith1126 <nsmiith8@gmail.com>
2025-09-03 18:50:54 -0400

    add misc functions

--- src/Utils/misc.py
+++ src/Utils/misc.py
@@ -11,6 +11,7 @@ from moveit_msgs.msg import Constraints, TrajectoryConstraints, PositionConstrai
 from std_msgs.msg import Header
 from shape_msgs.msg import SolidPrimitive
 from typing import Optional, Union, Sequence
+from moveit_commander import MoveGroupCommander, RobotCommander
 
 def init_scene_with_table(table_center: Optional[Union[Sequence[float], np.ndarray]] = None,
                             table_dims: Optional[tuple] = None) -> tuple:
@@ -66,3 +67,48 @@ def init_scene_with_table(table_center: Optional[Union[Sequence[float], np.ndarr
     
     return robot, manipulator, scene
     
+def add_elbow_constraints(manipulator: MoveGroupCommander, robot: RobotCommander) -> MoveGroupCommander:
+    '''
+    Adds elbow constraint to manipulator object to ensure Ur5e does not hit the table with any of it's arms
+    Inputs:
+    manipulator: MoveGroupCommander - UR5e planning object
+    Robot: RobotCommander - object for UR5e states and information
+    '''
+    
+    ws_box = SolidPrimitive()
+    ws_box.type = SolidPrimitive.BOX
+    ws_box.dimensions = [60*2/39.37, 60*2/39.37, 70/39.37]  # width, length, height
+
+    # Pose of the workspace box
+    ws_pose = Pose()
+    ws_pose.position.x = -(20/2 - 3.5)/39.37
+    ws_pose.position.y = 0.0
+    ws_pose.position.z = (70+2)/(2*39.37)
+
+    # Bounding volume
+    ws_region = BoundingVolume()
+    ws_region.primitives = [ws_box]
+    ws_region.primitive_poses = [ws_pose]
+
+    # Position constraint message
+    position_constraint = PositionConstraint()
+    position_constraint.header.frame_id = robot.get_planning_frame()
+    position_constraint.link_name = "forearm_link"  # elbow link
+    position_constraint.target_point_offset = Vector3(0.0, 0.0, -(2)/(2*39.37))
+    position_constraint.constraint_region = ws_region
+    position_constraint.weight = 1.0
+
+    # Full constraints
+    ws_constraint = Constraints()
+    ws_constraint.position_constraints = [position_constraint]
+
+    # Trajectory constraints
+    ws_traj_constraint = TrajectoryConstraints()
+    ws_traj_constraint.constraints = [ws_constraint]
+
+    # increase solving time 
+    manipulator.set_planning_time(15.0)
+
+    # Apply constraints to the manipulator
+    manipulator.set_path_constraints(ws_constraint)
+    return manipulator
